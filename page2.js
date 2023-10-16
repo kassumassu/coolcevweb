34530459e3173f5d7a3f236601d4e196452016df
kassumassu <142390041+kassumassu@users.noreply.github.com>
2023-10-16 20:10:19 +0300

    Add files via upload

---
+++ page2.js
@@ -0,0 +1,26 @@
+alert('there are nothing yet!') 
+var nimme = prompt('can you rate this website 0 to 5'); 
+if (nimme=="0") {
+    alert('okay you dont like this website');
+}  
+if (nimme=="1") {
+    confirm('are you sure but okay'); 
+} 
+if (nimme=="2") {
+    alert('okay'); 
+} 
+if (nimme=="3") {
+    alert('thanks'); 
+} 
+if (nimme=="4") {
+    alert('cool!'); 
+} 
+if (nimme=="5") {
+    alert("so cool thank you so much!"); 
+} 
+if (nimme >= 5) {
+    alert('okay i dont undersand'); 
+} 
+if (nimme <= 0) {
+    alert('what did you say?'); 
+}
---
+++ site.js
@@ -0,0 +1,11 @@
+alert('Welcome!') 
+nimi = prompt('What is your name') 
+if (nimi=="Caapo") {
+    alert('Tervetuloa omistaja' + " " + nimi) 
+} 
+else {
+    alert('Tervetuloa' + " " + nimi)
+} 
+function x() {
+    s=document.getElementById("a33").innerHTML="i hope you like also!" 
+} 
